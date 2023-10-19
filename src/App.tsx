964b6f5edfc4daf7ca8cdc8f49c8cdf45cd43adc
“Umarov <lilya1976.03.03@gmail.com>
2023-10-19 15:24:13 +0600

    сделал цикл для вывода цифр и кнопку

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,24 @@ import Number from "./Number/Number";
 const App = () => {
   const [numbers, setNumbers] = useState<number[]>([5, 11, 16, 23, 32]);
 
+  const displayNewNumbers = () => {
+    const newNumberToDisplay: number[] = [];
+
+    while (newNumberToDisplay) {
+        if (newNumberToDisplay.length > 5) {
+            break;
+        } else if (newNumberToDisplay.length <= 5) {
+            const newNumber: number = Math.floor(Math.random() * (36 - 5 + 1)) + 5;
+            newNumberToDisplay.push(newNumber);
+        }
+    }
+
+    setNumbers(newNumberToDisplay);
+  };
+
   return (
     <div className="container">
+      <div><button type="button" onClick={displayNewNumbers}>New numbers</button></div>
       <Number number={numbers[0]}/>
       <Number number={numbers[1]}/>
       <Number number={numbers[2]}/>
