0fd25db0783d08d697dbb3c36990cabc12209ffb
“Umarov <lilya1976.03.03@gmail.com>
2023-10-19 18:14:17 +0600

    сделал так чтобы числа не повторялись

--- src/App.tsx
+++ src/App.tsx
@@ -3,33 +3,38 @@ import {useState} from "react";
 import Number from "./Number/Number";
 
 const App = () => {
-  const [numbers, setNumbers] = useState<number[]>([5, 11, 16, 23, 32]);
+    const [numbers, setNumbers] = useState<number[]>([5, 11, 16, 23, 32]);
 
-  const displayNewNumbers = () => {
-    const newNumberToDisplay: number[] = [];
+    const displayNewNumbers = () => {
+        const newNumberToDisplay: number[] = [];
 
-    while (newNumberToDisplay) {
-        if (newNumberToDisplay.length > 5) {
-            break;
-        } else if (newNumberToDisplay.length <= 5) {
+        while (newNumberToDisplay) {
             const newNumber: number = Math.floor(Math.random() * (36 - 5 + 1)) + 5;
-            newNumberToDisplay.push(newNumber);
+            if (newNumberToDisplay.length > 5) {
+                break;
+            } else if (newNumberToDisplay.length <= 5) {
+                if (!newNumberToDisplay.includes(newNumber)) {
+                    newNumberToDisplay.push(newNumber);
+                    newNumberToDisplay.sort(function (a: number, b: number){
+                        return a - b;
+                    });
+
+                    setNumbers(newNumberToDisplay);
+                }
+            }
         }
-    }
+    };
 
-    setNumbers(newNumberToDisplay);
-  };
-
-  return (
-    <div className="container">
-      <div><button type="button" onClick={displayNewNumbers}>New numbers</button></div>
-      <Number number={numbers[0]}/>
-      <Number number={numbers[1]}/>
-      <Number number={numbers[2]}/>
-      <Number number={numbers[3]}/>
-      <Number number={numbers[4]}/>
-    </div>
-  )
+    return (
+        <div className="container">
+            <div><button type="button" onClick={displayNewNumbers}>New numbers</button></div>
+            <Number number={numbers[0]}/>
+            <Number number={numbers[1]}/>
+            <Number number={numbers[2]}/>
+            <Number number={numbers[3]}/>
+            <Number number={numbers[4]}/>
+        </div>
+    );
 };
 
-export default App
+export default App;
